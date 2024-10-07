a38f5b5dadc637aa444961722b290bf1cb8a5d32
Crosby Nash <40636134+cros-nash@users.noreply.github.com>
2024-10-07 15:32:43 -0400

    changed show_all to show_all_profiles.html

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 from .views import ShowAllView # our view class definition 
 
 urlpatterns = [
-    path('', ShowAllView.as_view(), name='show_all'), # generic class-based view
+    path('', ShowAllView.as_view(), name='show_all_profiles'), # generic class-based view
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,7 +5,7 @@ from .models import Profile
 class ShowAllView(ListView):
     '''Create a subclass of ListView to display all mini_fb profiles.'''
     model = Profile
-    template_name = 'mini_fb/show_all.html'
+    template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
 
     def get_context_data(self, **kwargs):
