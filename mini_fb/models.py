4fc82d89373305a59fef7f8671175f3d619f4434
Crosby Nash <40636134+cros-nash@users.noreply.github.com>
2024-10-28 12:46:06 -0400

    update

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -12,6 +12,16 @@ class Profile(models.Model):
         '''Return all of the status messages about this profile.'''
         statusmessage = StatusMessage.objects.filter(profile=self)
         return statusmessage
+    
+    def get_friends(self):
+        '''Return a list of friend's profiles associated with this profile.'''
+        friends_as_profile1 = Friend.objects.filter(profile1=self).values_list('profile2', flat=True)
+        
+        friends_as_profile2 = Friend.objects.filter(profile2=self).values_list('profile1', flat=True)
+
+        all_friends = Profile.objects.filter(id__in=friends_as_profile1.union(friends_as_profile2))
+
+        return list(all_friends)
 
 class StatusMessage(models.Model):
     profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
@@ -33,4 +43,13 @@ class Image(models.Model):
 
     def __str__(self):
         '''Return a string representation of the Image object.'''
-        return f'Image for StatusMessage {self.status_message.id} uploaded at {self.timestamp}'
+        return f'Image for StatusMessage {self.status_message.id} uploaded at {self.timestamp}' # type: ignore
+    
+class Friend(models.Model):
+    profile1 = models.ForeignKey(Profile, related_name="profile1", on_delete=models.CASCADE)
+    profile2 = models.ForeignKey(Profile, related_name="profile2", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        '''Return a string representation of the relationship between friends.'''
+        return f'{self.profile1.fname} {self.profile1.lname} & {self.profile2.fname} {self.profile2.lname}'
