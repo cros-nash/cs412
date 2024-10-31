6ac11dbd59a1716d3e1458add862025942e93d15
Crosby Nash <40636134+cros-nash@users.noreply.github.com>
2024-10-31 08:17:09 -0400

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path('delete_comment/<int:pk>', DeleteCommentView.as_view(), name='delete_comment'),
     
     # authentication views
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'), ## NEW
-    path('logout/', auth_views.LogoutView.as_view(next_page='show_all'), name='logout'), ## NEW
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='show_all'), name='logout'),
 ]
